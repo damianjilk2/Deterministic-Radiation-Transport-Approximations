02fff9a5b7397a828ac89b5c887b95a183662651
damianjilk2 <jilk2@wisc.edu>
2024-06-13 09:36:08 -0500

    Refactored code with enhanced docstrings and logging improvements
    - Improved docstrings for better clarity and consistency across functions.
    - Enhanced logging for better traceability and debugging.
    - Refined error handling mechanisms, especially in read_input and write_output functions.
    - Updated function comments to better reflect their roles and purposes.
    - Overall code cleanup for readability and maintainability.

--- drt_1d.py
+++ drt_1d.py
@@ -6,6 +6,7 @@ Author: Damian Jilk
 import numpy as np
 import json
 import csv
+import logging
 
 def construct_source_term(num_voxels:int, source_voxel_index:int, source_strength:float=1):
     """
@@ -19,13 +20,15 @@ def construct_source_term(num_voxels:int, source_voxel_index:int, source_strengt
     Returns:
     - numpy.ndarray: Source term vector.
     """
+    logging.debug("Constructing source term with %d voxels, source at index %d, strength %f", num_voxels, source_voxel_index, source_strength)
     s = np.zeros(num_voxels)
     s[source_voxel_index] = source_strength
+    logging.debug("Constructed source vector: %s", s)
     return s
 
 def calculate_K_trans(ri:int, rj:int, voxel_size:float, sigma_s_matrix:np.ndarray):
     """
-    Calculate the neutron transition probability. Used by the construct_transition_matrix function to build the K_trans matrix.
+    Calculate the streaming operator. Used by the construct_transition_matrix function to build the K_trans matrix.
 
     Parameters:
     - ri (int): Index of the starting voxel.
@@ -34,13 +37,17 @@ def calculate_K_trans(ri:int, rj:int, voxel_size:float, sigma_s_matrix:np.ndarra
     - sigma_s_matrix (numpy.ndarray): Diagonal matrix of scattering cross-sections.
 
     Returns:
-    - float: Neutron transition probability.
+    - float: Streaming operator. This represents the probability that a particle born at the left side of ri streams to the left side of rj without interacting.
     """
+    logging.debug("Calculating K_trans from voxel %d to voxel %d with voxel size %f", ri, rj, voxel_size)
+    min_index = min(ri, rj)
     min_index = min(ri, rj)
     max_index = max(ri, rj)
     distance = abs(ri - rj) * voxel_size
-    tau = np.sum(np.diag(sigma_s_matrix[min_index:max_index])) * voxel_size
-    return np.exp(-tau) / (4 * np.pi * distance**2)
+    tau = np.sum(np.diag(sigma_s_matrix)[min_index:max_index]) * voxel_size
+    streaming_operator = np.exp(-tau) / (4 * np.pi * distance**2)
+    logging.debug("Calculated K_trans: %f", streaming_operator)
+    return streaming_operator
 
 def construct_transition_matrix(num_voxels:int, voxel_size:float, sigma_s_matrix:np.ndarray):
     """
@@ -54,14 +61,16 @@ def construct_transition_matrix(num_voxels:int, voxel_size:float, sigma_s_matrix
     Returns:
     - numpy.ndarray: Transition matrix.
     """
+    logging.debug("Constructing transition matrix with %d voxels and voxel size %f", num_voxels, voxel_size)
     K_trans = np.zeros((num_voxels, num_voxels))
     for i in range(num_voxels):
         for j in range(num_voxels):
             if i == j:
-                # Set diagonal values to small positive value
-                K_trans[i, j] = 1e-20
+                # Set diagonal values to one based on defintion of streaming operator
+                K_trans[i, j] = 1
             else:
                 K_trans[i, j] = calculate_K_trans(i, j, voxel_size, sigma_s_matrix)
+    logging.debug("Constructed transition matrix: %s", K_trans)
     return K_trans
 
 def calculate_phi(K_trans:np.ndarray, sigma_s_matrix:np.ndarray, s:np.ndarray):
@@ -76,43 +85,111 @@ def calculate_phi(K_trans:np.ndarray, sigma_s_matrix:np.ndarray, s:np.ndarray):
     Returns:
     - numpy.ndarray: Neutron flux vector.
     """
+    logging.debug("Calculating neutron flux vector")
     phi = np.dot(np.linalg.inv(np.eye(len(K_trans)) - np.dot(K_trans, sigma_s_matrix)), np.dot(K_trans, s))
+    logging.debug("Calculated neutron flux vector: %s", phi)
     return phi
 
-def main():
-    # Load global variables from the JSON file
-    with open("input_data.json", "r") as json_file:
-        input_data = json.load(json_file)
+def read_input(file_path:str):
+    """
+    Reads input data from a specified JSON file.
 
+    Parameters:
+    - file_path (str): The path to the JSON file containing input data.
+
+    Returns:
+    - dict: A dictionary containing the input parameters loaded from the JSON file.
+    """
+    logging.debug("Reading input from %s", file_path)
+    try:
+        with open(file_path, "r") as json_file:
+            data = json.load(json_file)
+        logging.debug("Input data: %s", data)
+    except FileNotFoundError:
+        logging.error("File %s not found.", file_path)
+        raise
+    except json.JSONDecodeError:
+        logging.error("Error decoding JSON from file %s.", file_path)
+        raise
+    return data
+
+def perform_calculation(input_data:dict):
+    """
+    Performs the main calculation for the neutron flux based on input parameters.
+
+    Parameters:
+    - input_data (dict): A dictionary containing input parameters including spatial boundaries, voxel count, source index, and scattering cross-sections.
+
+    Returns:
+    - numpy.ndarray: The neutron flux vector calculated from the input data.
+    """
+    logging.debug("Performing calculation with input data: %s", input_data)
     x_min = input_data.get("minimum_x")
     x_max = input_data.get("maximum_x")
-    num_voxels = input_data.get("number_of_voxels") 
+    num_voxels = input_data.get("number_of_voxels")
+    source_voxel_index = input_data.get("source_voxel_index")
     sigma_s_values = input_data.get("scattering_cross_section")
     sigma_s_matrix = np.diag(sigma_s_values)
-    print("Scattering Cross-section Matrix (sigma_s):\n", sigma_s_matrix)
 
-    # Calculate voxel size
     voxel_size = (x_max - x_min) / num_voxels
-    
-    # Calculate source vector
-    s = construct_source_term(num_voxels, 2, 1) #currently using arbitrary values
-    print(f"Source Vector (s):\n{s}")
-    
-    # Calculate transition matrix
+    s = construct_source_term(num_voxels, source_voxel_index)
     K_trans = construct_transition_matrix(num_voxels, voxel_size, sigma_s_matrix)
-    print(f"Transition Matrix (K_trans):\n{K_trans}")
-    
-    # Calculate neutron flux vector
     phi = calculate_phi(K_trans, sigma_s_matrix, s)
-    print(f"Neutron Flux Vector (phi):\n{phi}")
     
-    # Write results to CSV file
-    with open("results.csv", "w", newline="") as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(["Voxel Index", "Neutron Flux"])
-        for i, flux in enumerate(phi):
-            writer.writerow([i, flux])
-    print("Results have been written to 'results.csv'.")
+    logging.info("Scattering Cross-section Matrix (sigma_s):\n%s", sigma_s_matrix)
+    logging.info("Source Vector (s):\n%s", s)
+    logging.info("Transition Matrix (K_trans):\n%s", K_trans)
+    logging.info("Neutron Flux Vector (phi):\n%s", phi)
+    
+    logging.debug("Calculation result (phi): %s", phi)
+    return phi
+
+def write_output(data:np.ndarray, file_path:str="results.csv"):
+    """
+    Writes the calculation results to a CSV file.
+
+    Parameters:
+    - data (numpy.ndarray): The neutron flux vector to be written to the file.
+    file_path (str, optional): The path to the CSV file where results will be saved, default is "results.csv".
+
+    Returns:
+    - None
+    """
+    logging.debug("Writing data to CSV file %s", file_path)
+    try:
+        with open(file_path, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Voxel Index", "Neutron Flux"])
+            for i, flux in enumerate(data):
+                writer.writerow([i, flux])
+        logging.info("Data written to CSV file %s", file_path)
+    except Exception as e:
+        logging.error("Error writing to CSV file: %s", e)
+        raise
+
+def setup_logging(level:int = logging.INFO):
+    """
+    Configures logging settings.
+
+    Parameters:
+    - level (int, optional): The logging level to set (e.g., logging.DEBUG, logging.INFO).
+
+    Returns:
+    - None
+    """
+    logging.basicConfig(
+        filename='drt_1d.log',
+        filemode='w',
+        level=level,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+def main():
+    setup_logging()
+    logging.debug("Main function started")
+    input_data = read_input("input_data.json")
+    phi = perform_calculation(input_data)
+    write_output(phi)
 
 if __name__ == "__main__":
     main()
